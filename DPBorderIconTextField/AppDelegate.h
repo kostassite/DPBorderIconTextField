910f709c2948eb340ae6a2140e29ae9177bed481
Kostas Antonopoulos <kostas@devpro.gr>
2016-12-22 17:14:49 +0200

    Initial Commit

---
+++ DPBorderIconTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DPBorderIconTextField
+//
+//  Created by Kostas on 22/12/16.
+//  Copyright © 2016 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DPBorderIconTextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DPBorderIconTextField
+//
+//  Created by Kostas on 22/12/16.
+//  Copyright © 2016 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
