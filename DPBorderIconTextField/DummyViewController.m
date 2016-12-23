4cf45fc2ee0b7fd44c684917a053df6c6dacc73e
Kostas Antonopoulos <kostas@devpro.gr>
2016-12-23 22:42:08 +0200

    Implemented textFieldShouldReturn in DummyVC.

--- DPBorderIconTextField/DummyViewController.m
+++ DPBorderIconTextField/DummyViewController.m
@@ -13,7 +13,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.borderIconTextField.textFieldDelegate = self;
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -21,14 +22,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - UITextFieldDelegate
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
 }
-*/
 
 @end
